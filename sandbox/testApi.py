db9ea959f0d8079949f392b9633d8689ee69149f
scottlow <slow@uvic.ca>
2015-03-07 16:30:58 -0800

    Scraped all the things.

--- sandbox/testApi.py
+++ sandbox/testApi.py
@@ -3,7 +3,27 @@ import sys
 import requests
 import json
 
-r = requests.get("https://api.fda.gov/device/event.json?limit=25")
+f = open('event_keys.txt', 'r')
+w = open('output.txt', 'w')
+event_keys = f.readlines()
 
-j = json.loads(r.content)
-print json.dumps(j)
+root = None
+first = False
+
+for event_key in event_keys:
+	event_key = event_key.strip()
+	r = requests.get("https://api.fda.gov/device/event.json?search=event_key:" + event_key)
+
+	if(first == False):
+		first = True
+		j = json.loads(r.content)
+		root = j["results"]
+		continue
+
+	root.append(j["results"][0])
+	print event_key
+
+json.dump(root, w, indent=4)
+
+f.close()
+w.close()
