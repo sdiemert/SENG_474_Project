92a482a704bad17c0cff4096886766552fb9da5d
scottlow <slow@uvic.ca>
2015-03-07 17:10:44 -0800

    Upgraded script
    We’re now displaying the running total of status codes we get in each
    case. The script now queries for device_event_keys as this was
    providing better results than previously. We’ll wait to hear back from
    Fieren as to what field we should be querying.

--- sandbox/testApi.py
+++ sandbox/testApi.py
@@ -2,6 +2,8 @@ import sys
 #import urllib2
 import requests
 import json
+from collections import Counter
+import time
 
 f = open('event_keys.txt', 'r')
 w = open('output.txt', 'w')
@@ -9,10 +11,16 @@ event_keys = f.readlines()
 
 root = None
 first = False
+codes = Counter()
 
 for event_key in event_keys:
 	event_key = event_key.strip()
-	r = requests.get("https://api.fda.gov/device/event.json?search=event_key:" + event_key)
+	r = requests.get("https://api.fda.gov/device/event.json?api_key=K81daldhUBV23QZmJMuVQBjwuNYm92Fmi9dDLjEv&search=event_key:" + event_key)
+
+	if(r.status_code != 200):
+		print r.status_code
+
+	codes[r.status_code] += 1
 
 	if(first == False):
 		first = True
@@ -23,7 +31,13 @@ for event_key in event_keys:
 	root.append(j["results"][0])
 	print event_key
 
+	time.sleep(0.5)
+
 json.dump(root, w, indent=4)
+
+print "Completed with the following response codes: "
+for k, v in codes.items():
+	print "\t" + str(k) + " : " + str(v)
 
 f.close()
 w.close()
